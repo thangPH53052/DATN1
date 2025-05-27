84eff395a89c58f5c6a18d697dd7036a4754c8ae
bm-dat42 <162992550+bm-dat42@users.noreply.github.com>
2025-05-27 23:05:55 +0700

    DatTH Auto commit at Tue 05/27/2025 23:05:55.61

--- src/main/java/org/example/datn/Controller/SanPhamController.java
+++ src/main/java/org/example/datn/Controller/SanPhamController.java
@@ -1,7 +1,13 @@
 package org.example.datn.Controller;
 
 
+import org.example.datn.Entity.ChatLieu;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.swing.text.AttributeSet;
+
 public class SanPhamController {
 
-
+    @Autowired
+    private ChatLieu chatLieu;
 }
