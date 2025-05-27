1b8dd778524d578631bbb876c85dc8759a2e4096
LuongDucThang <thangldph53052@gmail.com>
2025-05-27 23:24:40 +0700

    Auto commit at Tue 05/27/2025 23:24:40.85

--- src/main/java/org/example/datn/Controller/SanPhamController.java
+++ src/main/java/org/example/datn/Controller/SanPhamController.java
@@ -1,7 +1,7 @@
 package org.example.datn.Controller;
 
 
-<<<<<<< HEAD
+
 import org.example.datn.Entity.ChatLieu;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -9,13 +9,15 @@ import javax.swing.text.AttributeSet;
 
 public class SanPhamController {
 
-    @Autowired
-    private ChatLieu chatLieu;
-=======
-import org.springframework.beans.factory.annotation.Autowired;
+//    @Autowired
+//    private ChatLieu chatLieu;
+//=======
+//import org.springframework.beans.factory.annotation.Autowired;
+//
+//public class SanPhamController {
+//fdgdgdsgdfgds
+//>>>>>>> ab4b158 (Auto commit at Tue 05/27/2025 23:20:56.90)
+//        //esfeoifyeofyeofugeojfugeopjfugeopjfgep//
+//}
 
-public class SanPhamController {
-fdgdgdsgdfgds
->>>>>>> ab4b158 (Auto commit at Tue 05/27/2025 23:20:56.90)
-        rghdrsghersgohesoghtesohfgoesh
-}
+    }
